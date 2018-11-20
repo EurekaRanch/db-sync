45917196cec5f0f4265d68f588f5af1b12dd9482
dunkoh <scott@eurekaranch.com>
2018-11-20 14:46:05 -0500

    move delete db to after get remote data

--- database-update.sh
+++ database-update.sh
@@ -36,9 +36,6 @@ mkdir -p $CONFIG/dumps
 echo Backing up local database: $LOCAL_DATABASE_NAME
 mysqldump -v -h $LOCAL_DATABASE_HOST -u $LOCAL_DATABASE_USER -p$LOCAL_DATABASE_PASS $LOCAL_DATABASE_NAME > $CONFIG/dumps/local-database-$CURRENT_TIME.sql
 
-# Delete local database
-echo Dropping local database: $LOCAL_DATABASE_NAME
-mysqladmin -h $LOCAL_DATABASE_HOST -u $LOCAL_DATABASE_USER -p$LOCAL_DATABASE_PASS drop $LOCAL_DATABASE_NAME -f
 
 # Create local database
 echo Creating local database: $LOCAL_DATABASE_NAME
@@ -49,10 +46,14 @@ if mysql -h $LOCAL_DATABASE_HOST -u $LOCAL_DATABASE_USER -p$LOCAL_DATABASE_PASS 
 
 	# Download database dump
 	echo Exporting database \'$REMOTE_DATABASE_NAME\' from remote server: $REMOTE_DATABASE_HOST
-	mysqldump -v -h $REMOTE_DATABASE_HOST -u $REMOTE_DATABASE_USER -p$REMOTE_DATABASE_PASS $REMOTE_DATABASE_NAME > $CONFIG/dumps/remote-database-$CURRENT_TIME.sql
+	mysqldump -v --extended-insert -h $REMOTE_DATABASE_HOST -u $REMOTE_DATABASE_USER -p$REMOTE_DATABASE_PASS $REMOTE_DATABASE_NAME > $CONFIG/dumps/remote-database-$CURRENT_TIME.sql
 
 	echo Remote database exported: remote-database-$CURRENT_TIME.sql
-
+	
+	# Delete local database
+	echo Dropping local database: $LOCAL_DATABASE_NAME
+	mysqladmin -h $LOCAL_DATABASE_HOST -u $LOCAL_DATABASE_USER -p$LOCAL_DATABASE_PASS drop $LOCAL_DATABASE_NAME -f
+	
 	# Upload dump to local database
 	echo Importing database \'remote-database-$CURRENT_TIME.sql\' to local server: $LOCAL_DATABASE_HOST
 	mysql -h $LOCAL_DATABASE_HOST -u $LOCAL_DATABASE_USER -p$LOCAL_DATABASE_PASS $LOCAL_DATABASE_NAME < $CONFIG/dumps/remote-database-$CURRENT_TIME.sql
