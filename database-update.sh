31aa938c5f2be31e601a984074a7bb481cb87e23
dunkoh <scott@eurekaranch.com>
2018-11-20 20:01:55 -0500

    oops deleted db after creating. fixed that.

--- database-update.sh
+++ database-update.sh
@@ -36,11 +36,6 @@ mkdir -p $CONFIG/dumps
 echo Backing up local database: $LOCAL_DATABASE_NAME
 mysqldump -v -h $LOCAL_DATABASE_HOST -u $LOCAL_DATABASE_USER -p$LOCAL_DATABASE_PASS $LOCAL_DATABASE_NAME > $CONFIG/dumps/local-database-$CURRENT_TIME.sql
 
-
-# Create local database
-echo Creating local database: $LOCAL_DATABASE_NAME
-mysqladmin -h $LOCAL_DATABASE_HOST -u $LOCAL_DATABASE_USER -p$LOCAL_DATABASE_PASS create $LOCAL_DATABASE_NAME
-
 # Check for database connections
 if mysql -h $LOCAL_DATABASE_HOST -u $LOCAL_DATABASE_USER -p$LOCAL_DATABASE_PASS -e 'use '"$LOCAL_DATABASE_NAME" && mysql -h $REMOTE_DATABASE_HOST -u $REMOTE_DATABASE_USER -p$REMOTE_DATABASE_PASS -e 'use '"$REMOTE_DATABASE_NAME"; then
 
@@ -53,6 +48,10 @@ if mysql -h $LOCAL_DATABASE_HOST -u $LOCAL_DATABASE_USER -p$LOCAL_DATABASE_PASS 
 	# Delete local database
 	echo Dropping local database: $LOCAL_DATABASE_NAME
 	mysqladmin -h $LOCAL_DATABASE_HOST -u $LOCAL_DATABASE_USER -p$LOCAL_DATABASE_PASS drop $LOCAL_DATABASE_NAME -f
+	
+	# Create local database again
+	echo Creating local database: $LOCAL_DATABASE_NAME
+	mysqladmin -h $LOCAL_DATABASE_HOST -u $LOCAL_DATABASE_USER -p$LOCAL_DATABASE_PASS create $LOCAL_DATABASE_NAME
 	
 	# Upload dump to local database
 	echo Importing database \'remote-database-$CURRENT_TIME.sql\' to local server: $LOCAL_DATABASE_HOST
